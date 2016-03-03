a7c1bfe8918b09e9295e802351291fe112c45a06
Kevin Gill <hkevgill@hotmail.com>
2016-03-03 13:29:15 -0800

    Made ready queue store addresses

--- Project 2/os.c
+++ Project 2/os.c
@@ -93,7 +93,7 @@ typedef struct ProcessDescriptor {
   */
 static PD Process[MAXTHREAD];
 
-PD ReadyQueue[MAXTHREAD];
+volatile PD *ReadyQueue[MAXTHREAD];
 volatile int RQFront = 0;
 volatile int RQRear = 0;
 
@@ -148,7 +148,7 @@ int isEmpty() {
 /*
  *  Insert into the queue sorted by priority
  */
-void enqueueRQ(PD *p){
+void enqueueRQ(volatile PD **p){
     if(isFull()) {
         return;
     }
@@ -165,7 +165,7 @@ PD *dequeueRQ() {
         return;
     }
 
-    PD *result = &(ReadyQueue[RQFront]);
+    volatile PD *result = (ReadyQueue[RQFront]);
 
     RQFront = (RQFront + 1) % MAXTHREAD;
 
@@ -235,7 +235,7 @@ void Kernel_Create_Task_At( PD *p, voidfuncptr f, PRIORITY py, int arg ) {
     p->state = READY;
 
     // Add to ready queue
-    enqueueRQ(p);
+    enqueueRQ(&p);
 
 }
 
@@ -313,7 +313,7 @@ static void Next_Kernel_Request() {
         case NONE:
             /* NONE could be caused by a timer interrupt */
             Cp->state = READY;
-            enqueueRQ(Cp);
+            enqueueRQ(&Cp);
             Dispatch();
             break;
         case TERMINATE:

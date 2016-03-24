9906654298a1c22c68b6a373f3e7ddec53334bcd
Chris Cook <chriscook25@gmail.com>
2016-03-24 16:25:35 -0700

    more concise remote code

--- Project 3/remote_station/remote.c
+++ Project 3/remote_station/remote.c
@@ -7,39 +7,55 @@
 #include "../rtos/os.h"
 #include "../uart/uart.h"
 
-unsigned int ReceivePID;
 unsigned int IdlePID;
+unsigned int RoombaTestPID;
+unsigned int BluetoothReceivePID;
 
 unsigned char rbyte;
 
 // An idle task that runs when there is nothing else to do
 // Could be changed later to put CPU into low power state
-// void toggleLED() {
-//     PORTL ^= _BV(PORTL6);
-// }
 
-void Idle() {
-    for(;;) {
-    }
+void toggleLED() {
+	PORTL ^= _BV(PORTL6);
 }
+
+// ------------------------------ IDLE ------------------------------ //
+void Idle() {
+	for(;;) {
+		continue;
+	}
+}
+
+// ------------------------------ ROOMBA TEST ------------------------------ //
+void Roomba_Test() {
+	for(;;) {
+		Roomba_Drive(75,DRIVE_STRAIGHT);
+		_delay_ms(5000);
+		Roomba_Drive(-75,DRIVE_STRAIGHT);
+		_delay_ms(5000);
+	}
+}
+
+// ------------------------------ BLUETOOTH RECIEVE ------------------------------ //
+void Bluetooth_Receive() {
+	for(;;){
+		continue;
+	}
+}
+
 
 // Application level main function
 // Creates the required tasks and then terminates
 void a_main() {
 
-    IdlePID = Task_Create(Idle, MINPRIORITY, 1);
+	IdlePID = Task_Create(Idle, MINPRIORITY, 1);
+	RoombaTestPID = Task_Create(Roomba_Test, 9, 2);
+	BluetoothReceivePID = Task_Create(Bluetooth_Receive, 1, 3);
 
-    Bluetooth_UART_Init();
-    Roomba_UART_Init();
+	Bluetooth_UART_Init();
+	Roomba_UART_Init();
+	Roomba_Init();
 
-    Roomba_Init();
-
-    for(;;) {
-        Roomba_Drive(50,DRIVE_STRAIGHT);
-        _delay_ms(5000);
-        Roomba_Drive(-50,DRIVE_STRAIGHT);
-        _delay_ms(5000);
-    }
-
-    Task_Terminate();
+	Task_Terminate();
 }

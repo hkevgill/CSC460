6ce969334c06f13b332a952f2ce991016f2a40b2
Kevin Gill <hkevgill@hotmail.com>
2016-03-24 16:16:26 -0700

    joystick readings

--- Project 3/base_station/base.c
+++ Project 3/base_station/base.c
@@ -4,11 +4,12 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include "../roomba/roomba.h"
-#include "../roomba/roomba_sci.h"
 #include "../rtos/os.h"
 #include "../uart/uart.h"
 
 unsigned int IdlePID;
+
+int x, y = 0;
 
 // An idle task that runs when there is nothing else to do
 // Could be changed later to put CPU into low power state
@@ -27,6 +28,41 @@ void Send() {
     }
 }
 
+void InitADC() {
+    ADMUX |= (1<<REFS0);
+    ADCSRA|=(1<<ADEN)|(1<<ADPS0)|(1<<ADPS1)|(1<<ADPS2); //ENABLE ADC, PRESCALER 128
+}
+
+void ReadJoystick() {
+    for(;;) {
+
+        // Read x
+        ADMUX = (ADMUX & 0xE0); // Channel 8
+
+        ADCSRB |= (1<<MUX5);
+
+        ADCSRA |= (1<<ADSC); // Start conversion
+
+        while((ADCSRA)&(1<<ADSC));    //WAIT UNTIL CONVERSION IS COMPLETE
+
+        x = (ADC);
+
+        // Read y
+        ADMUX = (ADMUX & 0xE0); // Channel 8
+        ADMUX |= (1<<MUX0);
+
+        ADCSRB |= (1<<MUX5);
+
+        ADCSRA |= (1<<ADSC); // Start conversion
+
+        while((ADCSRA)&(1<<ADSC));    //WAIT UNTIL CONVERSION IS COMPLETE
+
+        y = ADC;
+
+        Task_Sleep(10);
+    }
+}
+
 // Application level main function
 // Creates the required tasks and then terminates
 void a_main() {
@@ -34,7 +70,10 @@ void a_main() {
 
     Bluetooth_UART_Init();
 
-    Task_Create(Send, 1, 1);
+    InitADC();
+
+    // Task_Create(Send, 5, 1);
+    Task_Create(ReadJoystick, 1, 1);
     IdlePID = Task_Create(Idle, MINPRIORITY, 1);
 
     Task_Terminate();

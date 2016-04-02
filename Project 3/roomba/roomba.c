4b1288d621d2cbff91623814118450306b160497
Chris Cook <chriscook25@gmail.com>
2016-04-02 15:40:18 -0700

    roomba song playing

--- Project 3/roomba/roomba.c
+++ Project 3/roomba/roomba.c
@@ -34,3 +34,27 @@ void Roomba_Drive(int16_t velocity, int16_t radius){
     Roomba_Send_Byte(radius>>8);
     Roomba_Send_Byte(radius);   
 }
+
+void Roomba_Play(uint8_t song) {
+	Roomba_Send_Byte(PLAY);
+	Roomba_Send_Byte(song);
+}
+
+void Roomba_Song(uint8_t n) {
+	Roomba_Send_Byte(SONG);
+	Roomba_Send_Byte(n);  // Song 0
+	Roomba_Send_Byte(6);  // 6 Notes
+	
+	Roomba_Send_Byte(72); // C
+	Roomba_Send_Byte(16);
+	Roomba_Send_Byte(69); // A
+	Roomba_Send_Byte(16);
+	Roomba_Send_Byte(67); // G
+	Roomba_Send_Byte(16);
+	Roomba_Send_Byte(64); // E
+	Roomba_Send_Byte(16);
+	Roomba_Send_Byte(62); // D
+	Roomba_Send_Byte(16);
+	Roomba_Send_Byte(60); // C
+	Roomba_Send_Byte(16);
+}
